5025eec06f7c466dc295a383bb760b49f155f120
Radek <radek.tesar135@gmail.com>
2024-04-07 19:42:39 +0200

    with statement - connection

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import smtplib
 
-my_email = "email@email.com"
+my_email = "email@gmail.com"
 password = "password"
-message = "Ahoj, posílám ti email"
+message = "Hello world!"
+received_email = "email@gmail.com"
 
+with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user= my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs=received_email,
+                        msg=message.encode("utf-8")
+                        )
