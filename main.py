e7c24cde10950da1eb08fcf10eaa105377408daf
Radek <radek.tesar135@gmail.com>
2024-04-07 19:56:21 +0200

    API

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import smtplib
+import requests
 
+# API
+response = requests.get(url="https://api.kanye.rest")
+data = response.json()
+quote = data["quote"]
+
+# Variables
 my_email = "email@gmail.com"
 password = "password"
 message = "Subject: Important message\n\nHello world!"
-received_email = "email@gmail.com"
+received_email = ["email@gmail.com", "nextemail@gmail.com"]
 
+# With statement
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
     connection.starttls()
     connection.login(user= my_email, password=password)
     connection.sendmail(from_addr=my_email,
                         to_addrs=received_email,
-                        msg=message.encode("utf-8")
+                        msg=quote.encode("utf-8")
                         )
